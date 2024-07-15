e37e60e72462ed1a6463f36c952c0ff1e6fe5a6f
eunseopsong <lexondms1@g.skku.edu>
2024-07-15 15:57:25 +0900

    백업 1

--- user/AiDIN_Controller/main.cpp
+++ user/AiDIN_Controller/main.cpp
@@ -8,16 +8,12 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "AiDIN_Controller.hpp"
-// #include "main_helper.h"
+#include "main_helper.h"
 
 int main(int argc, char** argv) {
-  // ROS2 초기화
   rclcpp::init(argc, argv);
-
-  // 기존 main_helper 함수 호출
-//   main_helper(argc, argv, new MIT_Controller()); // wbc_aidin/robot/src/main_helper.cpp
-
-  // ROS2 종료
+  main_helper(argc, argv, new AiDIN_Controller()); // wbc_aidin/robot/src/main_helper.cpp
   rclcpp::shutdown();
+
   return 0;
 }
